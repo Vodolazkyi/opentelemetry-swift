b7f2eae4348aa2499965d9b33c917e0211ccc2d9
Vodolazkyi <anton.vodolazkyi@gen.tech>
2024-04-11 00:39:42 +0300

    update packege config

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,9 +6,10 @@ import PackageDescription
 let package = Package(
     name: "opentelemetry-swift",
     platforms: [
-        .macOS(.v10_13),
-        .iOS(.v11),
-        .tvOS(.v11)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v5)
     ],
     products: [
         .library(name: "OpenTelemetryApi", type: .static, targets: ["OpenTelemetryApi"]),
@@ -30,14 +31,13 @@ let package = Package(
         .library(name: "NetworkStatus", type: .static, targets: ["NetworkStatus"]),
     ],
     dependencies: [
-        .package(name: "Opentracing", url: "https://github.com/undefinedlabs/opentracing-objc", exact: "0.5.2"),
-        .package(name: "Thrift", url: "https://github.com/undefinedlabs/Thrift-Swift", exact: "1.1.1"),
-        .package(name: "swift-nio", url: "https://github.com/apple/swift-nio.git", exact: "2.0.0"),
-        .package(name: "grpc-swift", url: "https://github.com/grpc/grpc-swift.git", exact: "1.0.0"),
-        .package(name: "swift-protobuf", url: "https://github.com/apple/swift-protobuf.git", exact: "1.20.2"),
-        .package(name: "swift-log", url: "https://github.com/apple/swift-log.git", exact: "1.4.4"),
-        .package(name: "swift-metrics", url: "https://github.com/apple/swift-metrics.git", exact: "2.1.1"),
-        .package(name: "Reachability.swift", url: "https://github.com/ashleymills/Reachability.swift", exact: "5.1.0")
+        .package(url: "https://github.com/undefinedlabs/opentracing-objc", from: "0.5.2"),
+        .package(url: "https://github.com/undefinedlabs/Thrift-Swift", from: "1.1.1"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.20.2"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.4"),
+        .package(url: "https://github.com/apple/swift-metrics.git", from: "2.1.1"),
     ],
     targets: [
         .target(name: "OpenTelemetryApi",
@@ -55,7 +55,6 @@ let package = Package(
         .target(name: "NetworkStatus",
                 dependencies: [
                     "OpenTelemetryApi",
-                    .product(name: "Reachability", package: "Reachability.swift")
                 ],
                 path: "Sources/Instrumentation/NetworkStatus",
                 linkerSettings: [.linkedFramework("CoreTelephony", .when(platforms: [.iOS], configuration: nil))]),
@@ -64,8 +63,10 @@ let package = Package(
                 path: "Sources/Instrumentation/SignPostIntegration",
                 exclude: ["README.md"]),
         .target(name: "OpenTracingShim",
-                dependencies: ["OpenTelemetrySdk",
-                               "Opentracing"],
+                dependencies: [
+                    "OpenTelemetrySdk",
+                    .product(name: "Opentracing", package: "opentracing-objc")
+                ],
                 path: "Sources/Importers/OpenTracingShim",
                 exclude: ["README.md"]),
         .target(name: "SwiftMetricsShim",
@@ -74,8 +75,10 @@ let package = Package(
                 path: "Sources/Importers/SwiftMetricsShim",
                 exclude: ["README.md"]),
         .target(name: "JaegerExporter",
-                dependencies: ["OpenTelemetrySdk",
-                               .product(name: "Thrift", package: "Thrift")],
+                dependencies: [
+                    "OpenTelemetrySdk",
+                    .product(name: "Thrift", package: "Thrift-Swift", condition: .when(platforms: [.iOS, .macOS, .tvOS, .macCatalyst, .linux]))
+                ],
                 path: "Sources/Exporters/Jaeger"),
         .target(name: "ZipkinExporter",
                 dependencies: ["OpenTelemetrySdk"],
